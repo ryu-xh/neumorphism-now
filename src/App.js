834b4fa78312687544c2c088072ab44211a4311a
RyuuseiKang <ryuuseikang@naver.com>
2020-02-01 00:53:21 +0900

    Add Github\

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './App.css';
 
+import { faHamburger } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 class App extends React.Component {
     constructor(props) {
     super(props);
@@ -18,8 +21,8 @@ class App extends React.Component {
   }
 
   _onMouseMove(e) {
-    this.setState({x: ((window.innerWidth - e.nativeEvent.offsetX) / window.innerWidth) - 0.5,
-                  y: ((window.innerHeight - e.nativeEvent.offsetY) / window.innerHeight) - 0.5});
+    this.setState({x: ((window.innerWidth - window.event.clientX) / window.innerWidth) - 0.5,
+                  y: ((window.innerHeight - window.event.clientY) / window.innerHeight) - 0.5});
     this.makeShadow();
   }
 
@@ -28,7 +31,7 @@ class App extends React.Component {
   
     this.setState({
       year: new Date().getFullYear().toString().substring(2, 4),
-      month: digit[parseInt(new Date().getMonth() + 1 / 10)] + (new Date().getMonth() + 1).toString(),
+      month: digit[parseInt((new Date().getMonth() + 1) / 10)] + (new Date().getMonth() + 1).toString(),
       day: digit[parseInt(new Date().getDate() / 10)] + new Date().getDate().toString(),
       hour: digit[parseInt(new Date().getHours() / 10)] + new Date().getHours().toString(),
       minutes: digit[parseInt(new Date().getMinutes() / 10)] + new Date().getMinutes().toString(),
@@ -58,9 +61,14 @@ class App extends React.Component {
     return (
       <div className="App">
         <header className="App-header" onMouseMove={this._onMouseMove.bind(this)} style={this.state.style}>
-          {this.state.year} {this.state.month} {this.state.day}<br/>
-          {this.state.hour} {this.state.minutes} {this.state.seconds}
+          <div>{this.state.year} {this.state.month} {this.state.day}</div>
+          <div>{this.state.hour}<span class="colon">:</span>{this.state.minutes}<span class="colon">:</span>{this.state.seconds}</div>
         </header>
+        <div class="footer" onMouseMove={this._onMouseMove.bind(this)} style={this.state.style}>
+          <a href="https://github.com/ryuuseikang/neumorphism-now">
+            <i class="fab fa-github fa-3x" onMouseMove={this._onMouseMove.bind(this)}></i>
+          </a>
+        </div>
       </div>
     );
   }
